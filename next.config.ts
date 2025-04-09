bffda5ed272a7605133356c3f2ed958365b89f60
aaloksg <56730032+aaloksg@users.noreply.github.com>
2025-04-09 11:26:52 +0200

    Sets up to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/tic-tac-toe/' : '',
+    basePath: isProd ? '/tic-tac-toe' : '',
+    output: 'export',
 };
 
 export default nextConfig;

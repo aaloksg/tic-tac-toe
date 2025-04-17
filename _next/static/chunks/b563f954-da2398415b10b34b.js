91b768075103574e8e382a0af82517fdaf16a64f
aaloksg <56730032+aaloksg@users.noreply.github.com>
2025-04-17 08:20:41 +0200

    Updates

---
+++ _next/static/chunks/b563f954-da2398415b10b34b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[585],{3671:(t,L,e)=>{e.d(L,{u0i:()=>i});var a=e(4436);function i(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M9,18 L9,12 M12,18 L12,13 M15,18 L15,10 M17,3 L21,3 L21,23 L3,23 L3,3 L7,3 M7,1 L17,1 L17,5 L7,5 L7,1 Z"},child:[]}]})(t)}}}]);
